416922011396687ba57cd0ee8de132a1b0fa287d
Saurabh Rajpurohit <purohitsraj52@gmail.com>
2020-08-27 10:28:13 +0530

    Updates

---
+++ precache-manifest.047827e7d70f8279119301aefc2276db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe957845171b3c266f08b0ff54d811a2",
+    "url": "/stackoverflow-micro-app/index.html"
+  },
+  {
+    "revision": "5e2b955d2617377ab345",
+    "url": "/stackoverflow-micro-app/static/css/main.45a1757a.chunk.css"
+  },
+  {
+    "revision": "3b429a24913a63097b03",
+    "url": "/stackoverflow-micro-app/static/js/2.a0a6f390.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stackoverflow-micro-app/static/js/2.a0a6f390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2b955d2617377ab345",
+    "url": "/stackoverflow-micro-app/static/js/main.885bf743.chunk.js"
+  },
+  {
+    "revision": "96412fc88c28f064e7f6",
+    "url": "/stackoverflow-micro-app/static/js/runtime-main.4faa7868.js"
+  }
+]);
